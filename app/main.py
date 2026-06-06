585306240b11b9c5fc8da9dd540a0eb163edf902
Priyansh_Porwal <priyanshporwal2004@gmail.com>
2026-06-07 03:23:52 +0530

    feat: Create routing

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
 
+from app.api.routes.health import router as health_router
+
 app = FastAPI(
-    title="Automated Invoice Reconciliation API"
+    title="Automated Invoice Reconciliation API",
+    version="0.1.0"
 )
+
 
 @app.get("/")
 def root():
-    return {"message": "Backend is running"}
+    return {
+        "message": "Backend is running"
+    }
+
+
+# Route Registration
+app.include_router(health_router)
